a40ce75b20b7af912b3834fb6b2afad8389b2ea5
han-pv <pirliyev17@gmail.com>
2026-02-07 10:30:26 +0500

    1.8.1

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -24,24 +24,4 @@ class Group extends Model
     protected $casts = [
         'days' => 'array',
     ];
-
-    public static function makeGroupCode($courseId, $courseName, $seasonId)
-    {
-        $group = Group::where("season_id", $seasonId)
-            ->where("course_id", $courseId)
-            ->latest()
-            ->first();
-
-        if (!$group) {
-            return $courseName[0] . "-A"; // E-A
-        }
-
-        $lastGroupCode = explode("-", $group->code); // E-B   => ["E", "B"]
-
-        $lastLetter = last($lastGroupCode); // "B"
-
-        $lastLetter++; //C
-
-        return $courseName[0] . "-" . $lastLetter; // E-C
-    }
 }
